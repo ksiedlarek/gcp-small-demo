48c3dbf4495be695404e68a4ba4a977b788730e9
Kasia Siedlarek <katsie@google.com>
2023-01-25 09:11:51 +0100

    clean up

--- config.tf
+++ config.tf
@@ -10,4 +10,11 @@ terraform {
     bucket = "fc4da6d62bc8b056-tfst"
     prefix = "terraform/demo-infra/tfstate"
   }
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.50.0"
+    }
+  }
 }
